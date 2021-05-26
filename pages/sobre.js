e164c3722413b24a7d99fae0640b0263499cd4fe
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 13:04:22 -0400

    primeira pagina

--- pages/sobre.js
+++ pages/sobre.js
@@ -3,7 +3,6 @@ function sobre () {
     return <div>
         <h1>Sobre</h1>
         <Link href="/"><a>Página principal</a></Link>
-            <Link href="/teste"><a>Página html</a></Link>
     </div>
 }
 export default sobre
