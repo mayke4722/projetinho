7d7a1abb3cfc2f03a3cea633aff96fbbb7f18848
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 17:32:54 -0400

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,14 @@ import Image from 'next/image';
 function home () {
     return <div>
         <head><title>Primeira página</title></head>
-        <h1 className="teste"><a className="teste" href="/sobre">Paginazinha </a></h1>
+        <h1><Link href="/sobre">
+        <a className="teste">Páginazinha</a>
+        </Link> </h1>
         <Image className="astolfo"
         src="/tolfin.png"
         width={390}
         height={516}
         />
-        <Link href="/sobre">
-        <a>Ir para a segunda página</a>
-        </Link>
     </div>
     
 }
