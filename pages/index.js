ff616843e127c24c6a71c22c1f29d60c01e707ff
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 12:36:16 -0400

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Link from 'next/link';
 function home () {
     return <div>
         <h1>Página Principal foda</h1>
-        <a href="/sobre">Acessar página sobre</a>
+        <Link href="/sobre">
+        <a>Acessar página sobre</a>
+        </Link>
     </div>
 }
 export default home
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,8 +1,8 @@
-
+import Link from 'next/link';
 function sobre () {
     return <div>
         <h1>Sobre</h1>
-
+        <Link href="/">Página principal</Link>
     </div>
 }
 export default sobre
