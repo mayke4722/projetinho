3455dd7aa33ec64d3313a2afe775930918617496
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 12:20:37 -0400

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
+import Link from 'next/link'
 function home () {
-    return <div>Nyago gostosa.</div>
+    return <div>
+        <h1>Página Principal</h1>
+        <link href="sobre.js">
+        <a>Acessar página sobre</a>
+        </link>
+    </div>
 }
 export default home
---
+++ pages/sobre.js
@@ -0,0 +1,11 @@
+
+function sobre () {
+    return <div>
+        <h1>Sobre</h1>
+        <link href="/">
+        <a>Acessar página home</a>
+        </link>
+
+    </div>
+}
+export default sobre
