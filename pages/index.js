5ae5779d6c7765aa40c343a076aab48b28d160e7
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 16:17:16 -0400

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ function home () {
     return <div>
         <head><title>Página Inicial</title></head>
         <h1 className="teste">Página Inicial</h1>
-        <Image
-        src="tolfin.png"
-        width={680}
-        height={500}
+        <Image className="floatRight"
+        src="/tolfin.png"
+        width={390}
+        height={516}
         />
         <Link href="/sobre">
         <a>Acessar página sobre</a>
