413fd1f3d2a3ccc0f9651f848f41882475dafd94
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 15:42:11 -0400

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ function home () {
     return <div>
         <head><title>Página Inicial</title></head>
         <h1 className="teste">Página Inicial</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/L9r-NxuYszg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <Link href="/sobre">
         <a>Acessar página sobre</a>
         </Link>
