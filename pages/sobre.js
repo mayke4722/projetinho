c937c2df1c142c39396562836c29ddd6ebb432ef
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 15:38:57 -0400

    primeira pagina

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
+import Head from 'next/head';
 function sobre () {
     return <div>
+        <head><title>Sobre</title></head>
+        <h1 className="primeiro">Página Sobre</h1>
         <Link href="/"><a>Voltar a página inicial</a></Link><br/>
     </div>
 }
