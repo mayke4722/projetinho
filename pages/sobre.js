bcc681c12091b1595830285b86da8a187411fe72
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 13:58:18 -0400

    primeira pagina

--- pages/sobre.js
+++ pages/sobre.js
@@ -3,6 +3,10 @@ function sobre () {
     return <div>
         <h1>Sobre</h1>
         <Link href="/"><a>Rique gostosa</a></Link>
+        <form>
+            <label>Rique gostosa?</label>
+            <input type="checkbox"/>sim
+        </form>
         <style jsx global>{`
   body {
     background: #EDC7B7};
