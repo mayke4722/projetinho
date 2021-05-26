4aee3c74fff72d3d602f40b1b82f11940d29ee01
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 14:18:05 -0400

    primeira pagina

--- pages/sobre.js
+++ pages/sobre.js
@@ -2,10 +2,9 @@ import Link from 'next/link';
 function sobre () {
     return <div>
         <form>
-            <label>Rique gostosa?</label><br/>
+            <label><h1>ash gostosa?</h1></label><br/>
             <input type="checkbox"/>sim<br/>
             <input type="checkbox"/>com certeza<br/>
-            <button type="submit"/>Enviar<br/>
         </form>
         <Link href="/"><a>Voltar a página inicial</a></Link><br/>
         <style jsx global>{`
