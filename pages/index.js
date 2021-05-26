3206270f14b48ca45c5416b2b24ccaaf98f8fc22
mayke4722 <maykeopenhs@outlook.com>
2021-05-26 13:40:21 -0400

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Link from 'next/link';
+import Head from 'next/head';
 function home () {
     return <div>
-
+        <head><title>Página Inicial</title></head>
         <h1>Página Principal foda</h1>
         <Link href="/sobre">
         <a>Acessar página sobre</a>
